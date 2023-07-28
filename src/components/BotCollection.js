09da37f09fa7899cff01bcffb75308ebe70efb36
JOY19ANYANGO <joyanyango402@gmail.com>
2023-07-28 11:46:16 +0300

    created db.json and added server script

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,29 +1,55 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
+import YourBotArmy from "./YourBotArmy";
 
-function  BotCollection(){
-    const [bots,setBots]=useState([])
-    useEffect(()=>{
-        fetch(' http://localhost:8001/bots')
-        .then((r)=>r.json())
-        .then((data)=>setBots(data))
-    },[])
-    return(
-        <div className="bot-container">
-        {bots.map((bot)=>(
-            <div key={bot.id} className="bot-frame">
-                <img src={bot.avatar_url}/>
-                <p>{bot.name}</p>
-                <p>{bot.health}</p>
-                <p>{bot.damage}</p>
-                <p>{bot.armor}</p>
-                <p>{bot.bot_class}</p>
-                <p>{bot.catchphrase}</p>
-                <p>{bot.created_at}</p>
-                <p>{bot.updated_at}</p>
+function BotCollection() {
+  const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([]);
 
-            </div>
+  useEffect(() => {
+    fetch('http://localhost:8001/bots')
+      .then((r) => r.json())
+      .then((data) => setBots(data));
+  }, []);
+
+  function handleClick(bot) {
+    // Check if the bot is already in the army by comparing their ids
+    const isBotInArmy = army.some((armyBot) => armyBot.id === bot.id);
+
+    if (!isBotInArmy) {
+      // If the bot is not in the army, add it to the army
+      setArmy([...army, bot]);
+    } else {
+      console.log('Bot is already in army');
+    }
+  }
+
+  function handleRemove(bot) {
+    // Remove the bot from the army
+    const updatedArmy = army.filter((armyBot) => armyBot.id !== bot.id);
+    setArmy(updatedArmy);
+  }
+
+  return (
+    <>
+      <YourBotArmy army={army} handleClickArmy={handleRemove} />
+      <h1>Bot Collection</h1>
+      <div className="bot-container">
+        {bots.map((bot) => (
+          <div key={bot.id} className="bot-frame" onClick={() => handleClick(bot)}>
+            <img src={bot.avatar_url} alt="" />
+            <p>{bot.name}</p>
+            <p>{bot.health}</p>
+            <p>{bot.damage}</p>
+            <p>{bot.armor}</p>
+            <p>{bot.bot_class}</p>
+            <p>{bot.catchphrase}</p>
+            <p>{bot.created_at}</p>
+            <p>{bot.updated_at}</p>
+          </div>
         ))}
-        </div>
-    )
+      </div>
+    </>
+  );
 }
-export default BotCollection
+
+export default BotCollection;
---
+++ src/components/YourBotArmy.js
@@ -0,0 +1,29 @@
+import React from "react";
+function YourBotArmy({army,handleClickArmy}){
+  
+   
+    return(
+        <>
+        <h1>Your Bot Army</h1>
+        <div className="bot-container">
+
+   {army.map((armyBot)=>(
+    <div key={armyBot.id} className="bot-frame" onClick={()=>handleClickArmy(armyBot)} >
+        <img src={armyBot.avatar_url} alt=""/>
+        <p>{armyBot.name}</p>
+        <p>{armyBot.health}</p>
+        <p>{armyBot.damage}</p>
+        <p>{armyBot.armor}</p>
+        <p>{armyBot.bot_class}</p>
+        <p>{armyBot.catchphrase}</p>
+        <p>{armyBot.created_at}</p>
+        <p>{armyBot.updated_at}</p>
+
+    </div>
+))}
+</div>
+
+        </>
+    )
+}
+export default YourBotArmy
